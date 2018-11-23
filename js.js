85660d104ac68d37b0d1aebdbff878e3a236a5fc
Alone <a.sinzig02@gmail.com>
2018-11-23 22:23:13 +0100

    sign in works now

--- js.js
+++ js.js
@@ -1,5 +1,9 @@
 function onSignIn(googleUser) {
+  document.getElementById("log").className = "logOut";
+  document.getElementById("log").setAttribute("onclick", "logOut();");
+  document.getElementById("log").innerHTML = "Ausloggen";
 
+  closeAlertBox();
 }
 
 function alertBox() {
@@ -11,6 +15,17 @@ function alertBox() {
         document.getElementById("contentHolder").addEventListener("click", closeAlertBox);
     }, 100);
 }
+
+function logOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut();
+
+  //auth2.disconnect();
+  setTimeout(function timeout() {
+    location.reload();
+  }, 100);
+}
+
 function closeAlertBox() {
     document.getElementById("alertBox").style.display = "none";
     document.getElementById("banner").style.opacity = "1";
